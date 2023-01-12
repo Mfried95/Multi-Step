b7454afc0ce1dad61d175907a813b71e00e07c9f
Mfried <81455905+Mfried95@users.noreply.github.com>
2023-01-12 11:11:00 -0600

    Installed styled components

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,28 @@
 import { useState } from 'react'
 import reactLogo from './assets/react.svg'
-import './App.css'
+import styled from "styled-components";
+import "./index.css"
 
 function App() {
   
   return (
     <div className="App">
-       
+      <FormContainer>
+
+      </FormContainer>
     </div>
   )
 }
 
 export default App
+
+
+
+
+const FormContainer = styled.div`
+  width: 1000px;
+  height: 600px;
+  background-color: blue;
+  display: flex;
+ margin:  5% auto;
+`;
