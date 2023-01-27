4e3b5917bc77c82dfa4449c243443d67785b7e30
Mfried95 <81455905+Mfried95@users.noreply.github.com>
2023-01-27 11:44:50 -0500

    Created a React hook for multistep
    MultiStep hook using state and calling it on App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -2,11 +2,11 @@ import { useState } from "react";
 import reactLogo from "./assets/react.svg";
 import styled from "styled-components";
 import "./index.css";
-import stepone from "./components/stepone";
-import steptwo from "./components/steptwo";
-import stepthree from "./components/stepthree";
-import stepfour from "./components/stepfour";
-
+import StepOne from "./components/stepone";
+import StepTwo from "./components/steptwo";
+import StepThree from "./components/stepthree";
+import StepFour from "./components/stepfour";
+import { useMultistepForm } from "./useMultistepForm";
 
 function App() {
   const [form, setform] = useState({
@@ -15,106 +15,21 @@ function App() {
     number: "",
   });
 
-  const [page, setPage] = useState(0);
-
-  const renderpage = () => {
-    if (page === 0) {
-      return stepOne();
-    } else if (page === 1) {
-      return stepTwo();
-    } else {
-      return stepThree()
-    }
-  };
-
-
-  const stepOne = () => {
-    return (
-      <>
-        <div>
-          <button
-            onClick={() => {
-              setPage(1);
-            }}
-          >
-            Next 
-          </button>
-          <h2>Step 1</h2>
-        </div>
-      </>
-    );
-  };
-
-  const stepTwo = () => {
-    return (
-      <>
-        <button
-          onClick={() => {
-            setPage(2);
-          }}
-        >
-          Next step
-        </button>
-        <button
-        onClick={() => {
-          setPage(0)
-        }}
-        >back</button>
-        <h2>Step 2</h2>
-      </>
-    );
-  };
-
-
-  const stepThree = () => {
-    return (
-      <>
-      <button>Next</button>
-      <button
-      onClick={() => {
-        setPage(1)
-      }}
-      >Back</button>
-      <h2>Step 3</h2>
-      </>
-    )
-  }
-
+  const {steps, currentStepIndex, next, back} = useMultistepForm([<StepOne/>, <StepTwo/>, <StepThree/>, <StepFour/>])
 
   return (
-    <div className="container">
-      <FormContainer>
-        <StepSelection></StepSelection>
-        <FormInfo>{renderpage()}</FormInfo>
-      </FormContainer>
+   <div>
+    {currentStepIndex + 1} / 0
+    {steps}
+    <div>
+    {currentStepIndex !== 0 && <button type="button" onClick={back}>Back</button>}
+    <button type="button" onClick={next}>Next</button>
     </div>
+   </div>
+   
   );
 }
 
 export default App;
 
-const StepSelection = styled.div`
-  background-image: url("../src/assets/images/bg-sidebar-desktop.svg");
-  background-repeat: no-repeat;
-  border-radius: 1em;
-  height: 100%;
-  width: 400px;
-  margin: 20px;
-  display: flex;
-  flex-direction: column;
-`;
 
-const FormContainer = styled.div`
-  width: 1000px;
-  height: 600px;
-  border-radius: 1.5em;
-  background-color: white;
-  display: flex;
-  margin: 5% auto;
-`;
-
-const FormInfo = styled.div`
-  width: 100%;
-  height: 100%;
-  background-color: white;
-`;
