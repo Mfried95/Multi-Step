552e3018c66eedb352ce362dd84e40e8d78166c9
Mfried95 <81455905+Mfried95@users.noreply.github.com>
2023-01-28 13:40:02 -0500

    Added sidebar step count

--- src/App.tsx
+++ src/App.tsx
@@ -13,21 +13,63 @@ function App() {
     number: "",
   });
 
-  const {steps, currentStepIndex, next, back} = useMultistepForm([<StepOne/>, <StepTwo/>, <StepThree/>, <StepFour/>])
+  const { steps, currentStepIndex, next, back } = useMultistepForm([
+    <StepOne />,
+    <StepTwo />,
+    <StepThree />,
+    <StepFour />,
+  ]);
 
   return (
-   <div>
-    <div className="form-container">
-      <div className="form-step-container">
-        
+    <div>
+      <div className="form-container">
+        <div className="count-container">
+          <div className="form-step-count">
+            <div className="form-count">
+              <div className="circle">
+                <div className="number">1</div>
+              </div>
+              <div className="count container">
+                <h2>Step 1</h2>
+                <p>YOUR INFO</p>
+              </div>
+            </div>
+
+            <div className="form-count">
+              <div className="circle">
+                <div className="number">2</div>
+              </div>
+              <div className="count container">
+                <h2>Step 2</h2>
+                <p>YOUR INFO</p>
+              </div>
+            </div>
+
+            <div className="form-count">
+              <div className="circle">
+                <div className="number">3</div>
+              </div>
+              <div className="count container">
+                <h2>Step 3</h2>
+                <p>YOUR INFO</p>
+              </div>
+            </div>
+
+            <div className="form-count">
+              <div className="circle">
+                <div className="number">4</div>
+              </div>
+              <div className="count container">
+                <h2>Step 4</h2>
+                <p>YOUR INFO</p>
+              </div>
+            </div>
+          </div>
+        </div>
+        <div className="step-container">{steps}</div>
       </div>
-      {steps}
     </div>
-   </div>
-   
   );
 }
 
 export default App;
-
-
