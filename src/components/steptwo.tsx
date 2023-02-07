132751f9e5579e33e0f8a871c64d6e28159cdcdf
Mfried95 <81455905+Mfried95@users.noreply.github.com>
2023-02-06 20:57:13 -0500

    Added step 2 buttons

--- src/components/steptwo.tsx
+++ src/components/steptwo.tsx
@@ -1,5 +1,8 @@
 import React from "react";
 import "../styles/steptwo.css";
+import Arcade from "../assets/images/icon-arcade.svg";
+import Advanced from "../assets/images/icon-advanced.svg";
+import Pro from "../assets/images/icon-pro.svg";
 
 type PlanData = {
   plan: string;
@@ -11,10 +14,6 @@ type PlanDataFormProps = PlanData & {
 };
 
 const steptwo = ({ plan, price, updateFields }: PlanDataFormProps) => {
-
-
-
-  
   return (
     <div>
       <div className="personal-info-container">
@@ -22,19 +21,35 @@ const steptwo = ({ plan, price, updateFields }: PlanDataFormProps) => {
           <h1>Select Your Plan</h1>
           <p>You have the option of Monthly or Yearly billing.</p>
         </div>
+
         <div className="plan-selection">
-          <section 
-          onClick={() => updateFields({ plan: "Arcade", price: +2 })}>
+          <button
+            type="button"
+            className="plan-btn"
+            onClick={() => updateFields({ plan: "Arcade", price: +2 })}
+          >
+            <img src={Arcade} alt="" />
             <h2>Arcade</h2>
-          </section>
-          <section
+            <span>$2 / monthly</span>
+          </button>
+          <button
+            type="button"
+            className="plan-btn"
             onClick={() => updateFields({ plan: "Advanced", price: +4 })}
           >
+            <img src={Advanced} alt="" />
             <h2>Advanced</h2>
-          </section>
-          <section onClick={() => updateFields({ plan: "Pro", price: +6 })}>
+            <span>$4 / monthly</span>
+          </button>
+          <button
+            type="button"
+            className="plan-btn"
+            onClick={() => updateFields({ plan: "Pro", price: +6 })}
+          >
+            <img src={Pro} alt="" />
             <h2>Pro</h2>
-          </section>
+            <span>$6 / monthly</span>
+          </button>
         </div>
       </div>
     </div>
